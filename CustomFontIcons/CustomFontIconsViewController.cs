317b78eb7bd53c848021f84fe7103c7acf62d3d5
Adam Patridge <patridge@gmail.com>
2014-05-15 09:01:10 -0600

    [fonts] Added use of font icon on a UIButton.

--- CustomFontIcons/CustomFontIconsViewController.cs
+++ CustomFontIcons/CustomFontIconsViewController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using MonoTouch.Foundation;
+using MonoTouch.CoreAnimation;
 
 namespace UIKitAbuse.CustomFontIcons {
     public class CustomFontIconsViewController : UIViewController {
@@ -14,13 +15,14 @@ namespace UIKitAbuse.CustomFontIcons {
             View.AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
 
             char jet = '\uf0fb';
-            var label = new UILabel(RectangleF.Empty) {
+            var label = new UILabel() {
                 Font = IconFont,
                 Text = jet.ToString(),
-                TextColor= UIColor.Orange,
-                BackgroundColor = UIColor.Blue,
+                TextColor= UIColor.Green,
+                BackgroundColor = UIColor.Gray,
             };
             label.SizeToFit();
+            label.Center = new PointF(View.Bounds.Width / 2f, View.Bounds.Width / 2f);
             Add(label);
 
             var slider = new UISlider(new RectangleF(new PointF(0f, View.Bounds.Height - 50f), new SizeF(View.Bounds.Width, 50f)));
@@ -31,8 +33,26 @@ namespace UIKitAbuse.CustomFontIcons {
             slider.ValueChanged += (sender, e) => {
                 label.Font = label.Font.WithSize(slider.Value);
                 label.SizeToFit();
+                label.Center = new PointF(View.Bounds.Width / 2f, View.Bounds.Width / 2f);
             };
             Add(slider);
+
+            char refresh = '\uf021';
+            var button = new UIButton(UIButtonType.RoundedRect) {
+                Font = IconFont,
+            };
+            button.SetTitle(refresh.ToString(), UIControlState.Normal);
+            button.SizeToFit();
+            button.Center = new PointF(View.Bounds.Width / 2f, slider.Frame.Top - (button.Frame.Height / 2f));
+            button.TouchUpInside += (sender, e) => {
+                // (Courtesy of http://www.patridgedev.com/2012/10/05/creating-an-animated-spinner-in-a-monotouch-uiimageview/)
+                CABasicAnimation rotationAnimation = CABasicAnimation.FromKeyPath("transform.rotation");
+                rotationAnimation.To = NSNumber.FromDouble(Math.PI * 2);
+                rotationAnimation.RemovedOnCompletion = true;
+                // Give the added animation a key for referencing it later (to remove, in this case).
+                label.Layer.AddAnimation(rotationAnimation, "rotationAnimation");
+            };
+            Add(button);
         }
         public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations() {
             return UIInterfaceOrientationMask.Portrait;
